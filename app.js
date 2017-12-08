491abaf21b66635f5344f42d9061385d10e4e4a9
Justin Kannekens <jpj.kannekens@student.avans.nl>
2017-12-08 17:33:03 +0100

    Hotfix

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 app.use(bodyParser.json());
+// CORS headers for local deploy
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 routes(app);
 
 app.use((err, req, res, next) => {
--- config/mongo.db.js
+++ config/mongo.db.js
@@ -4,7 +4,7 @@ const config = require('./env/env');
 // Gebruik es6 promises ipv mongoose mpromise
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config.dburl);
+mongoose.connect(config.dburl, {useMongoClient: true});
 var connection = mongoose.connection
     .once('open', () => console.log('Connected to Mongo on ' + config.dburl))
     .on('error', (error) => {
