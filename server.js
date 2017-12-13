8fc17ce4a8c0bdbee211677cec66ce6a692fc144
Justin Kannekens <jpj.kannekens@student.avans.nl>
2017-12-13 10:43:11 +0100

    Hotfix server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(logger('dev'));
 // CORS headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
